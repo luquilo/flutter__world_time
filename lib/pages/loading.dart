6b5d6186b580c562a79c3532dff3960983543ede
luquilo <luquilo.etc@gmail.com>
2023-03-12 20:37:36 +0700

    menampilkan datetime hasil dari request di endpoint api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert'; // used for converting json string
+import 'package:flutter__world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -10,49 +9,33 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    // learn how to make request on dummy api
+  String time = 'loading';
 
-    // http flutter package on version 0.13 update only accept uri
-    // using uri.parse to convert string to a uri
-    // returning a json data
-    // "Response" is given to us by the http module
-    // this response is getting a string that look like an object
-    // Response response =
-    //     await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-
-    // Map data = jsonDecode(response.body); //return a flutter map
-
-    // print(data['userId']);
-
-    // make a real request of london timezome
-    Response response = await get(
-        Uri.parse('https://worldtimeapi.org/api/timezone/Europe/London'));
-
-    Map data = jsonDecode(response.body);
-    print(data);
-
-    // get datetime and offset properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-
-    // create a datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-    // print(offset);
+  void setupWorldTime() async {
+    WorldTime contoh = WorldTime(
+        location: 'london', flag: 'england.png', url: 'Europe/London');
+    await contoh.getTime();
+    print(contoh.time);
+    print(contoh);
+    setState(() {
+      time = contoh.time;
+    });
+    // ini hanyalah code kosong
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart';
+import 'dart:convert'; // used for converting json string
+
+class WorldTime {
+  late String location; // location name for the ui
+  late String time; // the time in that location
+  late String flag; // an url to a flag icon
+  late String url; // the url of the api datetime endpoint
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    // make a real request of london timezome
+    Response response =
+        await get(Uri.parse('https://worldtimeapi.org/api/timezone/$url'));
+
+    Map data = jsonDecode(response.body);
+    print(data);
+
+    // get datetime and offset properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    // create a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    // print(now);
+    // print(offset);
+
+    // set the time variable property
+    time = now.toString();
+  }
+}
