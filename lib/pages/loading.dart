7a9be564b4222d14540477b0ae3afabd23f2d02f
luquilo <luquilo.etc@gmail.com>
2023-03-13 22:14:41 +0700

    make loading spin animation

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter__world_time/services/world_time.dart';
 import 'package:flutter__world_time/pages/home.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -20,10 +21,12 @@ class _LoadingState extends State<Loading> {
 
     // (pushReplacementNamed) digunakan untuk me replace layer loading dengan layer home
     //
-    Navigator.pushReplacementNamed(context, '/home', arguments: {
-      'location': contoh.location,
-      'flag': contoh.flag,
-      'time': contoh.time,
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, '/home', arguments: {
+        'location': contoh.location,
+        'flag': contoh.flag,
+        'time': contoh.time,
+      });
     });
   }
 
@@ -36,10 +39,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Text('loading'),
-      ),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+          child: SpinKitFadingCube(
+        color: Colors.white,
+        size: 80,
+      )),
     );
   }
 }
